01672036ad96daadb3e580f11806cd1cc41992a0
AkhilKurakayulu231099 <akhil.kurakayulu@infosys.com>
2025-02-15 12:40:54 +0530

    Update eon-home.js

--- blocks/eon-home/eon-home.js
+++ blocks/eon-home/eon-home.js
@@ -2,4 +2,7 @@ export default async function decorate(block) {
   var homeDiv1 = document.querySelector('.eon-home > div');
   homeDiv1.classList.add('eonhomeimg');
 
+  var homeDiv2 = document.querySelector('.eon-home > div:nth-child(2)');
+  homeDiv2.classList.add('eonhometransimg');
+
 }
