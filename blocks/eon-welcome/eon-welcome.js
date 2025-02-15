3159778abe52255cb6a3056c17d4541891c97153
AkhilKurakayulu231099 <akhil.kurakayulu@infosys.com>
2025-02-15 14:17:37 +0530

    Update eon-welcome.js

--- blocks/eon-welcome/eon-welcome.js
+++ blocks/eon-welcome/eon-welcome.js
@@ -1,7 +1,7 @@
 export default async function decorate(block) {
-  var homeDiv1 = document.querySelector('.eon-welcome > div');
+  var homeDiv1 = document.querySelector('.eon-welcome > div > div');
   homeDiv1.classList.add('eonwelcomeleft');
 
-  var homeDiv2 = document.querySelector('.eon-welcome> div:nth-child(2)');
+  var homeDiv2 = document.querySelector('.eon-welcome> div > div:nth-child(2)');
   homeDiv2.classList.add('eonwelcomeright');
 }
