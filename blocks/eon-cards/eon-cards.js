f3f475a7687675db91116095a7cf10dd4fab37fb
AkhilKurakayulu231099 <akhil.kurakayulu@infosys.com>
2025-02-15 16:05:41 +0530

    Update eon-cards.js

--- blocks/eon-cards/eon-cards.js
+++ blocks/eon-cards/eon-cards.js
@@ -1,13 +1,1 @@
-export default async function decorate(block) {
-  var maindiv = document.querySelector('.eon-cards > div');
-  maindiv.classList.add('cardsdiv');
 
-  var subdiv1 = document.querySelector('.eon-cards > div > div');
-  subdiv1.classList.add('cardimage');
-
-  var subdiv2 = document.querySelector('.eon-cards > div > div:nth-child(2)');
-  subdiv2.classList.add('cardheader');
-
-  var subdiv3 = document.querySelector('.eon-cards > div > div:nth-child(3)');
-  subdiv3.classList.add('cardlink');
-}
