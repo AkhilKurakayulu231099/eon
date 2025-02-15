eed68be357518293d7fa2385027ebe963eccbb36
AkhilKurakayulu231099 <akhil.kurakayulu@infosys.com>
2025-02-15 14:53:50 +0530

    Update eon-video.js

--- blocks/eon-video/eon-video.js
+++ blocks/eon-video/eon-video.js
@@ -1,7 +1,7 @@
 export default async function decorate(block) {
-  var homeDiv1 = document.querySelector('.eon-welcome > div > div');
-  homeDiv1.classList.add('eonwelcomeleft');
+  var homeDiv1 = document.querySelector('.eon-video > div > div');
+  homeDiv1.classList.add('lefttext');
 
-  var homeDiv2 = document.querySelector('.eon-welcome> div > div:nth-child(2)');
-  homeDiv2.classList.add('eonwelcomeright');
+  var homeDiv2 = document.querySelector('.eon-video > div > div:nth-child(2)');
+  homeDiv2.classList.add('rightvideo');
 }
