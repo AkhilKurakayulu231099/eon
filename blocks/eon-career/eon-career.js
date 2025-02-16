d0632eb627d8187b518f1b6be5936520f0ced160
AkhilKurakayulu231099 <akhil.kurakayulu@infosys.com>
2025-02-16 10:36:19 +0530

    Update eon-career.js

--- blocks/eon-career/eon-career.js
+++ blocks/eon-career/eon-career.js
@@ -1,7 +1,7 @@
 export default async function decorate(block) {
-  var homeDiv1 = document.querySelector('.eon-ceo > div > div');
-  homeDiv1.classList.add('eonceoimg');
+  var homeDiv1 = document.querySelector('.eon-career > div > div');
+  homeDiv1.classList.add('eoncareerimg');
 
-  var homeDiv2 = document.querySelector('.eon-ceo > div > div:nth-child(2)');
-  homeDiv2.classList.add('eonceotxt');
+  var homeDiv2 = document.querySelector('.eon-career > div > div:nth-child(2)');
+  homeDiv2.classList.add('eoncareertxt');
 }
